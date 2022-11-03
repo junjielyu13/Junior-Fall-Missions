2dd7497cbf914aac60f04968845f1593dd12ee9e
junjie_li <jfjunjieli@gmail.com>
2022-11-03 15:08:54 +0100

    update: p2-exercici 1

--- Xarxa/Practica/Practica_2/P2/exercici_1/exercici_1.ino
+++ Xarxa/Practica/Practica_2/P2/exercici_1/exercici_1.ino
@@ -1,5 +1,7 @@
 #include "ESP8266WiFi.h"
 
+int m = 0;
+
 void setup(){
   //put your setup code here, to run once:
   Serial.begin(9600);
@@ -14,24 +16,31 @@ void setup(){
 void loop(){
 
   Serial.println("Scan start");
-  
+  delay(10000);
+
+  Serial.print("Distancia: ");
+  m = Serial.readString().toInt();
+  Serial.print(m);
+  Serial.print("m :");
+
   int n = WiFi.scanNetworks();
   if(n==0)
     Serial.println("no networks found");
   else{
     for(int i = 0; i<n; i++){
-      Serial.print(i+1);
-      Serial.print(WiFi.SSID(i));
-      Serial.print("(");
-      Serial.print(WiFi.RSSI(i));
-      Serial.print(")");
-      Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*");
-      delay(10);
+      if (WiFi.SSID(i) == "MIWIFI_5G_bPkR"){
+        Serial.print(WiFi.SSID(i));
+        Serial.print("(");
+        Serial.print(WiFi.RSSI(i));
+        Serial.print(")");
+        Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*");
+      }
+      delay(100);
     }
   }
 
   Serial.println("*********************************");
   // wait a bit before scanning again
 
-  delay(5000);
+  delay(10000);
 }
